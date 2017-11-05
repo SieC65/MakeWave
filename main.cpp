d12d4d7d32a86973e69882cd038d9609de52642e
SieC65 <Shalexey91@mail.ru>
2017-11-06 01:34:34 +0300

    version 2.0

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 #include "MakeWave.h"
 #include <TF1.h>
 #include <TApplication.h>
+#include <TMath.h>
+
+//Time between center of gaus and trigger moment. Domain = 2*HalfDom.
+Double_t HalfDom (Double_t k, Double_t Width) {
+	Double_t HaD = Width*sqrt(log2(1/k))/2;
+	return HaD;
+}
 
 int main() {
 	TApplication *app = new TApplication("Canvas",0,0);
-//	TF1 *SPE = new TF1("SPE","((x > 0) && (x < 10))*(-0.001)");
-	TF1 *SPE = new TF1("SPE","gaus(0)",0,20);	//Function SPE, limits are set in ns
 	//Set parameters values
+	Double_t trig	 		= 0.01;		//trigger for SPE = trig*SPEAmplitude
 	Double_t SPEamplitude 	= (-0.001)*volt;	//Amplitude of SPE gaussian
 	Double_t SPEwidth 		= 10*ns;	//FWHM of SPE gaussian
 	Int_t Num 				= 100;		//Number of samples in OutWave
-	Double_t Delay 			= 0*ns;	//Delay from "0" of abs.time to "0" sample of OutWave
-	Double_t Period 		= 2*ns;	//Time between samples of OutWave
+	Double_t Delay 			= 0*ns;		//Delay from "0" of abs.time to "0" sample of OutWave
+	Double_t Period 		= 2*ns;		//Time between samples of OutWave
 	Double_t Gain 			= 0.125*(volt/1000);	//Units of ADC
 	//Let's create example of sequence of SPE's arrival times
 	Double_t TseqArr[] = { -5.1 , -3.0 , 1 , 2 , 22 , 24 , 44 , 48 , 68 , 74 , 94 , 102 , 122 , 132 , 152 , 164};
@@ -22,16 +28,19 @@ int main() {
 			TseqArr[i] *=ns;	//set times in ns
 			Tseq[i] = TseqArr[i];
 		}
-	SPE->SetParameter(0, SPEamplitude);			//Amplitude of gauss
-	SPE->SetParameter(1, SPEwidth/ns);			//Center of gauss
-	SPE->SetParameter(2, (SPEwidth/ns)/2.355);	//Sigma
+//	TF1 *SPE = new TF1("SPE","((x > 0) && (x < 10))*(-0.001)");
+	TF1 *SPE = new TF1("SPE","gaus(0)",0,(2*HalfDom(trig, SPEwidth)/ns));	//Function SPE, limits are set in ns
+	SPE->SetParameter(0, SPEamplitude);		//Amplitude of gauss
+	SPE->SetParameter(1, (HalfDom(trig, SPEwidth))/ns);	//Center
+	SPE->SetParameter(2, (SPEwidth/ns)/(2*sqrt(2*log(2))));	//Sigma
 	SPE->Draw();
+	cout << "SPE value at 0 = " << (SPE->Eval(0))/(0.001*volt) << "mV" << endl;
 	MakeWave *a = new MakeWave;			//Create object of MakeWave class
 	a->SetSPE (SPE);					//Set SPE function
 	a->SetParams (Num, Delay, Period, Gain);	//Set parameters
 	a->SetTimeSeq (&Tseq);				//Set sequence of SPE's arrival times
 	a->CreateOutWave();					//Create OutWave vector
-	a->PrintOutWave();					//Print OutWave
+//	a->PrintOutWave();					//Print OutWave
 	a->Draw();
 	return 0;
 }
